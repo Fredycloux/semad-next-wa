c0cf149f1a57f90961a7060253556fd02b470955
Fredycloux <clouxx@gmail.com>
2025-08-15 16:59:54 -0500

    Update route.js

--- semad-next-wa/src/app/api/admin/appointments/[id]/route.js
+++ semad-next-wa/src/app/api/admin/appointments/[id]/route.js
@@ -1,11 +1,10 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import { prisma } from "@/lib/prisma";
 
 export async function DELETE(_req, { params }) {
   try {
     await prisma.appointment.update({
       where: { id: params.id },
-      data: { status: "Cancelada" }, // o prisma.appointment.delete({ where: { id: params.id } })
+      data: { status: "Cancelada" },
     });
     return Response.json({ ok: true });
   } catch (e) {
