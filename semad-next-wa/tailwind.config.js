642efe074e0878065330f1a09c190d4272be8255
Fredycloux <clouxx@gmail.com>
2025-08-15 16:21:18 -0500

    Update tailwind.config.js

--- semad-next-wa/tailwind.config.js
+++ semad-next-wa/tailwind.config.js
@@ -3,4 +3,6 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: { extend: {} },
   plugins: [],
+  <div className="grid [grid-template-columns:repeat(16,minmax(0,1fr))] gap-2">
 };
+
