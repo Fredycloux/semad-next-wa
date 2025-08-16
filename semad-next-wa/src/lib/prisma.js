8e2347094182ed7d201a6a21f426c914136e0f16
Fredycloux <clouxx@gmail.com>
2025-08-15 19:47:29 -0500

    Update prisma.js

--- semad-next-wa/src/lib/prisma.js
+++ semad-next-wa/src/lib/prisma.js
@@ -1,9 +1,12 @@
 // src/lib/prisma.js
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-}
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
