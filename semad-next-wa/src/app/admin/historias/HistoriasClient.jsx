b5a8bf94af8fa1495cab3eb836929bfc7a6b2fde
Fredycloux <clouxx@gmail.com>
2025-08-28 00:48:05 -0500

    Update HistoriasClient.jsx

--- semad-next-wa/src/app/admin/historias/HistoriasClient.jsx
+++ semad-next-wa/src/app/admin/historias/HistoriasClient.jsx
@@ -3,7 +3,6 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import ConfirmDeleteButton from "@/components/ConfirmDeleteButton";
 
 export default function HistoriasClient({ q }) {
   const [patients, setPatients] = useState([]);
